e57f1bbf5b1076eda4aa9f032d7fa489ff955c56
Adrien Jalabert <35221770+ajalabert@users.noreply.github.com>
2019-02-28 14:12:54 +0100

    add coment to epxlain issue with deleted item in ion list

--- src/app/userTab/userTab.page.ts
+++ src/app/userTab/userTab.page.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { BookmarkedMedia } from './../../models/bookmarkedMedia';
 import { BookmarkService } from './../services/bookmark.service';
 import { Component } from '@angular/core';
@@ -11,19 +12,38 @@ export class UserTabPage {
 
   medias: BookmarkedMedia[];
 
-  constructor(private bookmarkService: BookmarkService) {}
+  constructor(private bookmarkService: BookmarkService, private router: Router) {}
 
   async ionViewWillEnter(){
     this.medias = await this.bookmarkService.getBookmarkedMedias();
     console.log(this.medias);
   }
 
-  onBookmarkedMediaClick(media){
-    // TODO Go to detail page?
+  async onBookmarkedMediaClick(media: BookmarkedMedia){
+    console.log(media.Type);
+    if (media.Type == "movie"){
+      await this.router.navigate(['/movie-detail/' + media.imdbID]);
+    }
+    else if (media.Type == "serie"){
+      await this.router.navigate(['/serie-detail/' + media.imdbID]);
+    }
+    else {
+      console.log("Unknown media Type (" + media.Type + ")");
+    }
   }
 
   async onBookmarkedMediaRemove(media){
+    // fix(item-sliding): Sliding no longer breaks after removing an item
+    // https://github.com/ionic-team/ionic/pull/17492/files
     await this.bookmarkService.removeFromBookmark(media);
     this.medias = await this.bookmarkService.getBookmarkedMedias();
+  }
+
+  async onImportClicked(){
+    
+  }
+
+  async onExportClicked(){
+
   }
 }
