38566509641c4a35ec1ac4a2543e644f0ae29e90
Adrien Jalabert <35221770+ajalabert@users.noreply.github.com>
2019-03-07 14:46:14 +0100

    add button instead of link fo export browser

--- src/app/userTab/userTab.page.ts
+++ src/app/userTab/userTab.page.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 import { Router } from '@angular/router';
 import { BookmarkedMedia } from './../../models/bookmarkedMedia';
 import { BookmarkService } from './../services/bookmark.service';
@@ -19,6 +20,8 @@ export class UserTabPage {
   async ionViewWillEnter(){
     this.medias = await this.bookmarkService.getBookmarkedMedias();
     console.log(this.medias);
+    await this.setHrefExportJsonBrowser();
+    await this.setHrefExportCsvBrowser();
   }
 
   async onBookmarkedMediaClick(media: BookmarkedMedia){
@@ -41,21 +44,27 @@ export class UserTabPage {
     this.medias = await this.bookmarkService.getBookmarkedMedias();
   }
 
-  async onImportClicked(event){
-    var file = event.target.files[0] as File;
-    const data = await new Response(file).text();
+  async onImportClicked(event) {
+    try {
+      var file = event.target.files[0] as File;
+      const data = await new Response(file).text();
 
-    if (file.type == "text/xml") {
-      console.log("upload xml");
-      await this.importXml(data);
+      console.log("format=" + file.type);
+
+      if (file.type == "text/csv" || "application/vnd.ms-excel") {
+        console.log("upload csv");
+        await this.importCsv(data);
+      }
+      else if (file.type == "application/json"){
+        console.log("upload json");
+        await this.importJson(data);
+      }
+      else {
+        console.log("unkown format");
+      }
+    } catch (error) {
+      console.log(error);
     }
-    else if (file.type == "application/json"){
-      console.log("upload json");
-      await this.importJson(data);
-    }
-    else {
-      console.log("unkown format");
-    }  
   }
 
   async onExportJsonClicked(){
@@ -75,8 +84,9 @@ export class UserTabPage {
     await this.addToBookMarck(medias);
   }
 
-  async importXml(data: string){
-    var medias = papa.parse(data).data;
+  async importCsv(data: string){
+    var medias = papa.parse(data, {header:true}).data;
+    console.log(medias);
     await this.addToBookMarck(medias);
   }
 
@@ -87,5 +97,21 @@ export class UserTabPage {
         await this.bookmarkService.saveToBookmark(media);
       }     
     }
+  }
+
+  async setHrefExportJsonBrowser() {
+    var medias = await this.bookmarkService.getBookmarkedMedias();
+    var json = JSON.stringify(medias);
+    var data = "application/json;charset=utf-8," + encodeURIComponent(json);
+    var element = document.getElementById("exportJsonButton");
+    element.setAttribute("href", "data:" + data);    
+  }
+
+  async setHrefExportCsvBrowser() {
+    var medias = await this.bookmarkService.getBookmarkedMedias();
+    var csv = papa.unparse(medias);
+    var data = "text/csv;charset=utf-8," + encodeURIComponent(csv);
+    var element = document.getElementById("exportCsvButton");
+    element.setAttribute("href", "data:" + data);
   }
 }
