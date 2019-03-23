2883033c17cf028e6144c6dde5245b08dfcae7bb
Adrien Jalabert <35221770+ajalabert@users.noreply.github.com>
2019-03-23 14:04:58 +0100

    fix issue with back button that doesn't work with tabs

--- src/app/pages/details/serie-detail/serie-detail.module.ts
+++ src/app/pages/details/serie-detail/serie-detail.module.ts
@@ -3,6 +3,7 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { BackButtonTabsModule } from 'ion-back-button-tabs';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -21,6 +22,7 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     ComponentsModule,
+    BackButtonTabsModule,
     RouterModule.forChild(routes)
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
